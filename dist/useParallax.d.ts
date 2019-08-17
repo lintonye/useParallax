8a51599bc67e3c33f0244ff3c8a06edac005a556
Linton Ye <linton@jimulabs.com>
2019-08-17 16:52:52 -0700

    Remove dist

--- dist/useParallax.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function usePositiveOffset(offset: any): import("framer-motion").MotionValue<any>;
-export declare function useSpeed(positiveOffset: any, ...rangeSpeedPairs: any[]): import("framer-motion").MotionValue<any>;
-export declare function useSticky(positiveOffset: any, ...ranges: any[]): import("framer-motion").MotionValue<any>;
-export declare function useParallax(positiveOffset: any, ...rangeFunPairs: any[]): import("framer-motion").MotionValue<any>;
-export declare function useTrigger(positiveOffset: any, range: any, actionFun: any): void;
